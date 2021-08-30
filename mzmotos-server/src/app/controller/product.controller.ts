526f0b05b02fa22d730f6b559623d29e91d2c217
Francisco David Muñoz Armas <fdmunoz2@espe.edu.ec>
2021-08-30 08:09:15 -0500

    Implementing the product controller

---
+++ mzmotos-server/src/app/controller/product.controller.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from "express";
+import { Product } from "../interfaces/product";
+import productSchema from "../schemas/product.schema";
+
+export const getProducts = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const products = await productSchema.find();
+        return res.status(200).json(products);
+    } catch (error: any) {
+        return res.status(500).json({ message: "error", error: error });
+    }
+}
+export const deleteProducts = async (req: Request, res: Response) => {
+    try {
+        await productSchema.deleteMany({});
+        return res.status(200).json({ message: "All items have been deleted"});
+    } catch (error: any) {
+        return res.status(500).json({ message: "error", error: error });
+    }
+}
+
+export const postProduct = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const product = await productSchema.create(req.body);
+        return res.status(200).json(product);
+    } catch (error: any) {
+        return res.status(500).json({ message: "error", error: error });
+    }
+}
+
+export const getProduct = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const product = await productSchema.findById(id);
+        if (product) {
+            return res.status(200).json(product);
+        }
+        return res.status(200).json({ message: "Data not found" });
+    } catch (error: any) {
+        return res.status(500).json({ message: "error", error: error });
+    }
+}
+
+export const putProducts = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const product = await productSchema.findByIdAndUpdate(id, req.body, { new: true });
+        return res.status(200).json(product);
+    } catch (error: any) {
+        return res.status(500).json({ message: "error", error: error });
+    }
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        await productSchema.findByIdAndRemove(id);
+        return res.status(200).json({ message: "Product deleted", itemID: id});
+    } catch (error: any) {
+        return res.status(500).json({ message: "error", error: error });
+    }
+}
---
+++ mzmotos-server/src/app/routes/product.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import * as Controller from '../controller/product.controller';
+
+const router = Router();
+
+router.route("/")
+.get(Controller.getProducts)
+.post(Controller.postProduct)
+.delete(Controller.deleteProducts);
+
+router.route("/:id")
+.get(Controller.getProducts)
+.put(Controller.putProducts)
+.delete(Controller.deleteProduct);

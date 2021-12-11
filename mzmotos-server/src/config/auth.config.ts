a8e3fd903152ac6dec01e4a61bd9f49cf15ec5df
Francisco David Muñoz Armas <panchitodmun@gmail.com>
2021-12-11 14:42:23 -0500

    Configuration, token, and auth

---
+++ mzmotos-server/src/config/auth.config.ts
@@ -0,0 +1,49 @@
+import { NextFunction, Request, Response } from "express";
+import { decode } from "../lib/token";
+
+const veriftyToken = (auth: any) => {
+  if (!auth) {
+    return false;
+  }
+  const bearer = auth.split(" ");
+  if (bearer[1] === null || !bearer[1]) {
+    return false;
+  }
+  const payload: any = decode(bearer[1]);
+  if (!payload) {
+    return false;
+  }
+  return payload;
+};
+/*
+const verifyExistance = async (payload: any) => {
+  try {
+    const docResult = await getDoc(doc(database, 'users', payload));
+    if (docResult.data()) {
+      return true;
+    }
+  } catch (error: any) {
+    return null;
+  }
+  return null;
+
+}
+*/
+export const authUser = (req: Request | any, res: Response, next: NextFunction) => {
+  const payload = veriftyToken(req.headers.authorization);
+  if (!payload) {
+    return res.status(401).json({ status: false, authorization: "Denied" });
+  }
+  next();
+  /*verifyExistance(payload)
+    .then((result) => {
+      if (result) {
+        req.payload = payload;
+        next();
+      } else {
+        return res.status(401).json({ status: false, authorization: "Denied" });
+      }
+    }).catch(() => {
+      return res.status(401).json({ status: false, authorization: "Denied" });
+    });*/
+};
--- mzmotos-server/src/lib/constants.ts
+++ mzmotos-server/src/lib/constants.ts
@@ -7,5 +7,6 @@ export default {
     MONGO_PASSWORD: process.env.MONGO_PASSWORD,
     MONGO_HOST: process.env.MONGO_HOST,
     MONGO_NAME: process.env.MONGO_NAME,
-    PORT: process.env.PORT || 3000
+    PORT: process.env.PORT || 3000,
+    SECRET_KEY: process.env.SECRET_KEY,
 }

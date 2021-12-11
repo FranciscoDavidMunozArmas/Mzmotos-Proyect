0ad1072b1c790206f3812fd5bb004a6dbf2b2aeb
Francisco David Muñoz Armas <panchitodmun@gmail.com>
2021-12-11 16:11:07 -0500

    appointment controller

--- mzmotos-server/src/app/controller/appointment.controller.ts
+++ mzmotos-server/src/app/controller/appointment.controller.ts
@@ -1,24 +1,15 @@
 import { Request, Response } from "express";
-import { Appointment, appointmentConverter } from "../model/appointment";
-import { salesmanConverter } from "../model/salesman";
+import { appointmentConverter } from "../interface/appointment.interface";
+import { salesmanConverter } from "../interface/salesman.interface";
 import salesmanSchema from "../schemas/salesman.schema";
 
 export const getAppointments = async (req: Request, res: Response) => {
     try {
         const { salesmanid } = req.params;
         const mongoData = await salesmanSchema.findById(salesmanid);
-        const appointments = salesmanConverter.fromJSON(mongoData)
-            .appointments.map(appointment => appointmentConverter.toJSON(appointment));
+        const appointments = salesmanConverter.convertJSON(mongoData)
+            .appointments.map((appointment: any) => appointmentConverter.convertJSON(appointment));
         return res.status(200).json(appointments);
-    } catch (error: any) {
-        return res.status(500).json({ message: "error", error: error });
-    }
-}
-export const deleteAppointments = async (req: Request, res: Response) => {
-    try {
-        const { salesmanid } = req.params;
-        const salesman = await salesmanSchema.findByIdAndUpdate(salesmanid, { $pull: { appointments: {} } }, { new: true });
-        return res.status(200).json(salesman?.appointments);
     } catch (error: any) {
         return res.status(500).json({ message: "error", error: error });
     }
@@ -27,7 +18,7 @@ export const deleteAppointments = async (req: Request, res: Response) => {
 export const postAppointment = async (req: Request, res: Response) => {
     try {
         const { salesmanid } = req.params;
-        const appointment: Appointment = appointmentConverter.toJSON(appointmentConverter.fromJSON(req.body));
+        const appointment = appointmentConverter.convertJSON(req.body);
         const updateSalesman = await salesmanSchema.findByIdAndUpdate(salesmanid, {
             $push: {
                 appointments: [appointment]
@@ -39,12 +30,24 @@ export const postAppointment = async (req: Request, res: Response) => {
     }
 }
 
+export const deleteAppointments = async (req: Request, res: Response) => {
+    try {
+        const { salesmanid } = req.params;
+        const salesman = await salesmanSchema.findByIdAndUpdate(salesmanid, { $pull: { appointments: {} } }, { new: true });
+        return res.status(200).json(salesman?.appointments);
+    } catch (error: any) {
+        return res.status(500).json({ message: "error", error: error });
+    }
+}
+
 export const getAppointment = async (req: Request, res: Response) => {
     try {
         const { salesmanid, appointmentid } = req.params;
         const salesman = await salesmanSchema.findById(salesmanid);
         if (salesman) {
-            const appointment = salesman.appointments.find((element: Appointment) => element._id == appointmentid);
+            const appointment = salesman.appointments.find((element: any) => {
+                if(element._id == appointmentid) return appointmentConverter.convertJSON(element);
+            });
             return res.status(200).json(appointment);
         }
         return res.status(200).json({ message: "Data not found" });
@@ -56,7 +59,7 @@ export const getAppointment = async (req: Request, res: Response) => {
 export const putAppointments = async (req: Request, res: Response) => {
     try {
         const { salesmanid, appointmentid } = req.params;
-        const appointment: Appointment = req.body;
+        const appointment = appointmentConverter.convertJSON(req.body);
         const salesman = await salesmanSchema.findOneAndUpdate(
             { _id: salesmanid, "appointments._id": appointmentid },
             { $set: { "appointments.$": appointment } },
@@ -70,11 +73,11 @@ export const putAppointments = async (req: Request, res: Response) => {
 export const deleteAppointment = async (req: Request, res: Response) => {
     try {
         const { salesmanid, appointmentid } = req.params;
-        const salesman = await salesmanSchema.findOneAndUpdate(
+        await salesmanSchema.findOneAndUpdate(
             { _id: salesmanid },
             { $pull: { appointments: { _id: appointmentid } } },
             { new: true });
-        return res.status(200).json({ message: "Appointment deleted", itemID: appointmentid });
+        return res.status(200).json({ message: "Appointment deleted" });
     } catch (error: any) {
         return res.status(500).json({ message: "error", error: error });
     }

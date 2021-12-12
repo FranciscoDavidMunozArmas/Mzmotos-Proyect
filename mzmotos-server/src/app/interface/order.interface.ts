9e6c11c68939a82922d7fed80d506b613e7fc392
Francisco David Muñoz Armas <panchitodmun@gmail.com>
2021-12-12 13:05:11 -0500

    Order interface

---
+++ mzmotos-server/src/app/interface/order.interface.ts
@@ -0,0 +1,39 @@
+import { Document } from "mongoose";
+import { Client, clientConverter } from "./client.interface";
+import { Product, productConverter } from "./product.interface";
+
+interface ProductOrder {
+    product: Product;
+    quantity: number;
+}
+
+const productOrderConverter = {
+    convertJSON: (json: any) => {
+        return {
+            product: productConverter.convertJSON(json.product),
+            quantity: json.quantity
+        };
+    },
+}
+
+export interface Order extends Document {
+    _id?: string,
+    id: number,
+    date: Date,
+    client: Client,
+    list: ProductOrder[],
+    total: number
+}
+
+export const orderConverter = {
+    convertJSON: (json: any) => {
+        return {
+            _id: json._id,
+            id: json.id,
+            date: json.date,
+            client: clientConverter.convertJSON(json.client),
+            list: json.list.map(productOrderConverter.convertJSON),
+            total: json.total
+        }
+    }
+}

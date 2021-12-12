2a5fba27b315ad1e8f181eb5a786a221b2bb835c
Francisco David Muñoz Armas <panchitodmun@gmail.com>
2021-12-12 11:28:46 -0500

    Manager routes

--- mzmotos-server/src/app/routes/manager.routes.ts
+++ mzmotos-server/src/app/routes/manager.routes.ts
@@ -1,6 +1,20 @@
 import { Router } from 'express';
-import * as Controller from '../controller/client.controller';
+import { authUser } from '../../config/auth.config';
+import * as Controller from '../controller/manager.controller';
 
 const router = Router();
+
+router.route("/")
+.get(authUser, Controller.getManagers)
+.post(Controller.createManager)
+.delete(authUser, Controller.deleteManagers);
+
+router.route("/one/:id")
+.get(authUser, Controller.getManagerByID)
+.put(authUser, Controller.updateManager)
+.delete(authUser, Controller.deleteManagerByID);
+
+router.route("/username/:username")
+.get(authUser, Controller.getManagerByUsername)
 
 export default router;

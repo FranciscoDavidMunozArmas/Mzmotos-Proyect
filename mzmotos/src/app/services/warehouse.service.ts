2db90cbe324f2e7bcebea5a5bf9322e47b8df438
Francisco David Muñoz Armas <panchitodmun@gmail.com>
2021-12-20 09:34:48 -0500

    Update warehouse service

--- mzmotos/src/app/services/warehouse.service.ts
+++ mzmotos/src/app/services/warehouse.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CONSTANTS } from 'src/lib/constants';
+import { Warehouse, warehouseConverter } from '../models/Warehouser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WarehouseService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWarehouses() {
+    return this.http.get(`${CONSTANTS.API_URL}/warehouses`);
+  }
+
+  postWarehouse(warehouse: Warehouse) {
+    return this.http.post(`${CONSTANTS.API_URL}/warehouses`, warehouseConverter.toJSON(warehouse));
+  }
+
+  deleteWarehouses() {
+    return this.http.delete(`${CONSTANTS.API_URL}/warehouses`);
+  }
+
+  getWarehouseByID(id: string) {
+    return this.http.get(`${CONSTANTS.API_URL}/warehouses/one/${id}`);
+  }
+
+  putWarehouseByID(id: string, warehouse: Warehouse) {
+    return this.http.put(`${CONSTANTS.API_URL}/warehouses/one/${id}`, warehouseConverter.toJSON(warehouse));
+  }
+
+  deleteWarehouseByID(id: string) {
+    return this.http.delete(`${CONSTANTS.API_URL}/warehouses/one/${id}`);
+  }
+
+  getWarehouseByUsename(usename: string) {
+    return this.http.get(`${CONSTANTS.API_URL}/warehouses/username/${usename}`);
+  }
 }
